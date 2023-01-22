59a005e33fd925fb82154d6a51173942a73f2d55
Adrian Torres <adrianluistorres2@gmail.com>
2023-01-21 23:03:07 -0500

    Subiendo proyecto al repositorio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link} from "react-router-dom";
+import Home from './Views/Home';
+import Num4 from './Components/Num4'
+import Hello from './Components/hello';
+import Bluered from './Components/Bluered';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+      <p>
+        <Link to="/home">Home</Link>
+        <Link to="/4">Page4</Link>
+        <Link to="/hello">Hello</Link>
+        <Link to="/hello/blue/red">bluered</Link>
+      </p>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/4' element={<Num4 />} />
+        <Route path='/hello' element={<Hello />} />
+        <Route path='/hello/blue/red' element={<Bluered />} />
+      </Routes>
+   </div>
   );
 }
 
---
+++ src/Components/Num4.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Num4 = () => {
+    
+    return (
+        <div>
+            <h1>The number is: 4</h1>            
+        </div>
+    )
+}
+
+export default Num4;
